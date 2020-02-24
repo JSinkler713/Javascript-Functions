3270a451e9cb18c96567298fa463d1f53d1b3f95
James Sinkler <jamessinkler713@gmail.com>
2020-02-24 07:55:49 -0800

    add two functions to declare and call

---
+++ javascript-functions-assignment.js
@@ -0,0 +1,21 @@
+// Write a function that has two parameters and sums them together
+
+function addNums(x, y) {
+  //write your code here
+  
+  //DON'T forget to return the value
+}
+
+
+//call the function with the arguments 3 and 5
+addNums(/*Your code here*/)
+
+//************************************************************
+// Declare a function named hello, that has a parameter name, that then returns 'HELLO (name)'
+
+function // Your code here
+
+
+// Call the function passing in your own name as an argument
+// Your code here
+
